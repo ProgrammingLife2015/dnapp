f927ac8a61ae40b82bb44d7024caab59d19f3006
maartentbm <M.H.Flikkema@student.tudelft.nl>
2015-04-28 18:37:47 +0200

    BinaryTree class from http://www.cs.dartmouth.edu/~cbk/10/notes/7/code/BinaryTree.java imported and refactored. Not yet working.

---
+++ src/main/java/nl/tudelft/ti2806/pl1/main/BinaryTree.java
@@ -0,0 +1,291 @@
+package nl.tudelft.ti2806.pl1.main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * Generic binary tree, storing data of a parametric data in each node.
+ * http://www.cs.dartmouth.edu/~cbk/10/notes/7/code/BinaryTree.java
+ * 
+ * @author Chris Bailey-Kellogg, Dartmouth CS 10, Fall 2012
+ * 
+ * @param <E>
+ *            the type of the data the tree carries.
+ */
+public class BinaryTree<E> {
+
+	/**
+	 * 
+	 */
+	private final BinaryTree<E> left, right; // children; can be null
+
+	/**
+	 * 
+	 */
+	private E data;
+
+	/**
+	 * Constructs leaf node with no children.
+	 * 
+	 * @param dataIn
+	 *            the data to carry
+	 */
+	public BinaryTree(final E dataIn) {
+		this.data = dataIn;
+		this.left = null;
+		this.right = null;
+	}
+
+	/**
+	 * Constructs inner node.
+	 * 
+	 * @param dataIn
+	 *            the data to carry
+	 * @param leftIn
+	 *            the left child
+	 * @param rightIn
+	 *            the right child
+	 */
+	public BinaryTree(final E dataIn, final BinaryTree<E> leftIn,
+			final BinaryTree<E> rightIn) {
+		this.data = dataIn;
+		this.left = leftIn;
+		this.right = rightIn;
+	}
+
+	/**
+	 * @return true iff this is an inner node
+	 */
+	public final boolean isInner() {
+		return left != null || right != null;
+	}
+
+	/**
+	 * @return true iff this is a leaf
+	 */
+	public final boolean isLeaf() {
+		return left == null && right == null;
+	}
+
+	/**
+	 * @return true iff this node has a left child
+	 */
+	public final boolean hasLeft() {
+		return left != null;
+	}
+
+	/**
+	 * @return true iff this node has a right child
+	 */
+	public final boolean hasRight() {
+		return right != null;
+	}
+
+	/**
+	 * 
+	 * @return the number of nodes (inner and leaf) in tree.
+	 */
+	public final int size() {
+		int num = 1;
+		if (hasLeft()) {
+			num += left.size();
+		}
+		if (hasRight()) {
+			num += right.size();
+		}
+		return num;
+	}
+
+	/**
+	 * 
+	 * @return the longest length to a leaf node from here
+	 */
+	public final int height() {
+		if (isLeaf()) {
+			return 0;
+		}
+		int h = 0;
+		if (hasLeft()) {
+			h = Math.max(h, left.height());
+		}
+		if (hasRight()) {
+			h = Math.max(h, right.height());
+		}
+		return h + 1; // inner: one higher than highest child
+	}
+
+	/**
+	 * Checks whether two trees are equal.
+	 * 
+	 * @param t2
+	 *            the tree to compare with
+	 * @return true iff this tree is equal to t2
+	 */
+	public final boolean equalsTree(final BinaryTree<E> t2) {
+		if (hasLeft() != t2.hasLeft() || hasRight() != t2.hasRight()) {
+			return false;
+		}
+		if (!data.equals(t2.data)) {
+			return false;
+		}
+		if (hasLeft() && !left.equalsTree(t2.left)) {
+			return false;
+		}
+		if (hasRight() && !right.equalsTree(t2.right)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @return leaves in order from left to right
+	 */
+	public final ArrayList<E> fringe() {
+		final ArrayList<E> f = new ArrayList<E>();
+		addToFringe(f);
+		return f;
+	}
+
+	/**
+	 * Helper for fringe, adding fringe data to the list
+	 */
+
+	/**
+	 * Adds this node to a list if it is a leave, else the same recursively for
+	 * its two children.
+	 * 
+	 * @param fringe
+	 *            the list to add to
+	 */
+	public final void addToFringe(final ArrayList<E> fringe) {
+		if (isLeaf()) {
+			fringe.add(data);
+		} else {
+			if (hasLeft()) {
+				left.addToFringe(fringe);
+			}
+			if (hasRight()) {
+				right.addToFringe(fringe);
+			}
+		}
+	}
+
+	@Override
+	public final String toString() {
+		return toStringHelper("");
+	}
+
+	/**
+	 * Recursively constructs a String representation of the tree from this
+	 * node, starting with the given indentation and indenting further going
+	 * down the tree.
+	 * 
+	 * @param indent
+	 *            indentation
+	 * @return a visual text representation of the tree
+	 */
+	public final String toStringHelper(final String indent) {
+		String res = indent + data + "\n";
+		if (hasLeft()) {
+			res += left.toStringHelper(indent + "  ");
+		}
+		if (hasRight()) {
+			res += right.toStringHelper(indent + "  ");
+		}
+		return res;
+	}
+
+	/**
+	 * Very simplistic binary tree parser based on Newick representation.
+	 * Assumes that each node is given a label that becomes the data. Any
+	 * distance information (following the colon) is stripped [tree] = "("
+	 * [tree] "," [tree] ")" [label] [":"[dist]] | [label] [":"[dist]] No effort
+	 * at all to handle malformed trees or those not following these strict
+	 * requirements.
+	 * 
+	 * @param s
+	 *            the string to parse
+	 * @return a phylogenetic tree
+	 */
+	public static BinaryTree<String> parseNewick(final String s) {
+		final BinaryTree<String> t = parseNewick(new StringTokenizer(s, "(,)",
+				true));
+		// Get rid of the semicolon
+		t.data = t.data.substring(0, t.data.length() - 1);
+		return t;
+	}
+
+	/**
+	 * Does the real work of parsing, now given a tokenizer for the string.
+	 * 
+	 * @param st
+	 *            a string tokenizer
+	 * @return a phylogenetic tree
+	 */
+	public static BinaryTree<String> parseNewick(final StringTokenizer st) {
+		final String token = st.nextToken();
+		if (token.equals("(")) {
+			// Inner node
+			final BinaryTree<String> left = parseNewick(st);
+			st.nextToken(); // final String comma = st.nextToken();
+			final BinaryTree<String> right = parseNewick(st);
+			st.nextToken(); // final String close = st.nextToken();
+			final String label = st.nextToken();
+			final String[] pieces = label.split(":");
+			return new BinaryTree<String>(pieces[0], left, right);
+		} else {
+			// Leaf
+			final String[] pieces = token.split(":");
+			return new BinaryTree<String>(pieces[0]);
+		}
+	}
+
+	/**
+	 * Copies the entire file content into a single String, and returns it.
+	 * 
+	 * @param filename
+	 *            relative path to the file to read
+	 * @return the file content as a continuous string
+	 * @throws IOException
+	 *             when something goes wrong xD
+	 */
+	public static String readIntoString(final String filename)
+			throws IOException {
+		final StringBuffer buff = new StringBuffer();
+		final BufferedReader in = new BufferedReader(new FileReader(filename));
+		String line;
+		while ((line = in.readLine()) != null) {
+			buff.append(line);
+		}
+		in.close();
+		return buff.toString();
+	}
+
+	/**
+	 * Some tree testing.
+	 * 
+	 * @param args
+	 *            jwz
+	 * @throws IOException
+	 *             when something goes wrong xD
+	 */
+	public static void main(final String[] args) throws IOException {
+		// Tree of life
+		final String s = readIntoString("inputs/itol.txt");
+		final BinaryTree<String> t = parseNewick(s);
+		System.out.println(t);
+		System.out.println("height:" + t.height());
+		System.out.println("size:" + t.size());
+
+		// Smaller trees
+		final BinaryTree<String> t1 = parseNewick("((a,b)c,(d,e)f)g;");
+		System.out.println("fringe:" + t1.fringe());
+
+		final BinaryTree<String> t2 = parseNewick("((a,b)c,(d,e)f)g;");
+		final BinaryTree<String> t3 = parseNewick("((a,b)z,(d,e)f)g;");
+		System.out.println("== " + t1.equalsTree(t2) + " " + t1.equalsTree(t3));
+	}
+}
