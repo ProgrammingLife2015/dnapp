b9e3dc395f520bd43fccbf87ad0806aa477f4a96
maartentbm <M.H.Flikkema@student.tudelft.nl>
2015-05-12 13:11:00 +0200

    Native look and feel.

--- src/main/java/nl/tudelft/ti2806/pl1/main/Start.java
+++ src/main/java/nl/tudelft/ti2806/pl1/main/Start.java
@@ -1,6 +1,7 @@
 package nl.tudelft.ti2806.pl1.main;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 import nl.tudelft.ti2806.pl1.gui.Window;
 
@@ -23,6 +24,12 @@ public final class Start {
 	public static void main(final String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager
+							.getSystemLookAndFeelClassName());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 				// UIManager.put("swing.boldMetal", Boolean.FALSE);
 				new Window();
 			}
