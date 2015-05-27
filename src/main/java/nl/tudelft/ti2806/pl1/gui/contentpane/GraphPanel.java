286a553c5c456d6b6d9cf3cdd062be23ad9efbae
Keraito <dragoon_v2_chak_shun@hotmail.com>
2015-05-27 11:19:18 +0200

    Fixed some javadoc in DNode and GraphPanel class

--- src/main/java/nl/tudelft/ti2806/pl1/gui/contentpane/GraphPanel.java
+++ src/main/java/nl/tudelft/ti2806/pl1/gui/contentpane/GraphPanel.java
@@ -99,6 +99,7 @@ public class GraphPanel extends JSplitPane implements NodeSelectionObserver {
 	/** The DGraph data storage. */
 	private DGraph dgraph;
 
+	/** A collection of the highlighted genomes. */
 	private Set<String> highlightedGenomes;
 
 	/**
@@ -291,6 +292,13 @@ public class GraphPanel extends JSplitPane implements NodeSelectionObserver {
 
 	}
 
+	/**
+	 * Assigns the css class for a node being selected and stores the old one.
+	 * Also restores the css for the previous selected node.
+	 * 
+	 * @param newSelectedNode
+	 *            Newly selected node.
+	 */
 	public final void selectNode(final Node newSelectedNode) {
 		// Restores the old class of the previous selected node if present.
 		if (selectedNode != null) {
@@ -307,7 +315,10 @@ public class GraphPanel extends JSplitPane implements NodeSelectionObserver {
 	}
 
 	/**
-	 * Highlights a genome. TODO debug code delete
+	 * Highlights a genome.
+	 * 
+	 * @param genomeId
+	 *            Id of the genome to be highlighted.
 	 */
 	public final void highlight(final String genomeId) {
 		for (DNode n : dgraph.getReferences().get(genomeId)) {
@@ -315,15 +326,16 @@ public class GraphPanel extends JSplitPane implements NodeSelectionObserver {
 					"highlight");
 		}
 		highlightedGenomes.add(genomeId);
-		System.out.println(highlightedGenomes.toString());
 	}
 
 	/**
-	 * Unhighlights a genome. TODO debug code delete
+	 * Unhighlights a genome.
+	 * 
+	 * @param genomeId
+	 *            Id of the genome to be highlighted.
 	 */
 	public final void unHighlight(final String genomeId) {
 		highlightedGenomes.remove(genomeId);
-		System.out.println(highlightedGenomes.toString());
 		for (DNode n : dgraph.getReferences().get(genomeId)) {
 			boolean contains = false;
 			for (String source : n.getSources()) {
