d8ab8d5f06635f4f8c406a92c7be4ad504e0083a
Keraito <dragoon_v2_chak_shun@hotmail.com>
2015-05-28 20:28:22 +0200

    Implement Start and End DNode class

---
+++ src/main/java/nl/tudelft/ti2806/pl1/DGraph/EndDNode.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package nl.tudelft.ti2806.pl1.DGraph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Special DNode data class for the added End Node.
+ * 
+ * @author Chak Shun
+ *
+ */
+public class EndDNode extends AbstractDNode {
+
+	/** Text for the special node to show. */
+	private String specialText = "END NODE";
+
+	/**
+	 * Constructor for the special DNode StartDnode.
+	 * 
+	 * @param idIn
+	 *            ID of the node.
+	 * @param sourcesIn
+	 *            Genomes which contains this node.
+	 * @param startIn
+	 *            Starting point in reference genome.
+	 * @param endIn
+	 *            End point in the reference genome.
+	 */
+	public EndDNode(final int idIn, final HashSet<String> sourcesIn,
+			final int startIn, final int endIn) {
+		super(idIn, sourcesIn, startIn, endIn);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public final String showContent() {
+		return specialText;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public final HashMap<String, String> getContentMap() {
+		HashMap<String, String> contentMap = new HashMap<String, String>();
+		for (String source : this.getSources()) {
+			contentMap.put(source, specialText);
+		}
+		return contentMap;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public final HashMap<String, Double> getPercUnknownMap() {
+		HashMap<String, Double> percentagesMap = new HashMap<String, Double>();
+		for (String source : this.getSources()) {
+			percentagesMap.put(source, 1.0);
+		}
+		return percentagesMap;
+	}
+
+}
