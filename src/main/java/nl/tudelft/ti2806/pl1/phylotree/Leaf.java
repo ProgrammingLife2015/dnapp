092082715d5fa22af0b9427e216913cbfc68812c
justinvdh <J.R.T.E.vanderhout@student.tudelft.nl>
2015-05-29 11:32:57 +0200

    fix highlighting leaf nodes in phylotree

--- src/main/java/nl/tudelft/ti2806/pl1/phylotree/Leaf.java
+++ src/main/java/nl/tudelft/ti2806/pl1/phylotree/Leaf.java
@@ -1,8 +1,6 @@
 package nl.tudelft.ti2806.pl1.phylotree;
 
 import java.awt.Color;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,7 +40,6 @@ public class Leaf extends BinaryTree {
 	 */
 	public Leaf(final String nameIn, final double dist, final PhyloPanel panel) {
 		super(nameIn, dist, panel);
-		addMouseListener(new LeafNodeMouseListener());
 	}
 
 	/**
@@ -129,42 +126,6 @@ public class Leaf extends BinaryTree {
 			return PhyloPanel.NODE_SELECTED_COLOR;
 		}
 		return PhyloPanel.DEFAULT_COLOR;
-	}
-
-	/**
-	 * @author Maarten, Justin
-	 * @since 28-5-2015
-	 * @version 1.0
-	 */
-	class LeafNodeMouseListener implements MouseListener {
-
-		/** {@inheritDoc} */
-		public void mouseClicked(final MouseEvent e) {
-		}
-
-		/** {@inheritDoc} */
-		public void mousePressed(final MouseEvent e) {
-			switch (e.getButton()) {
-			case MouseEvent.BUTTON1:
-				setChosen(!isChosen());
-				getPhyloPanel().plotTree();
-				break;
-			default:
-			}
-		}
-
-		/** {@inheritDoc} */
-		public void mouseReleased(final MouseEvent e) {
-		}
-
-		/** {@inheritDoc} */
-		public void mouseEntered(final MouseEvent e) {
-		}
-
-		/** {@inheritDoc} */
-		public void mouseExited(final MouseEvent e) {
-		}
-
 	}
 
 }
