ee624d1d7e59b905b158f1e019a6541dd06fb37e
maartentbm <M.H.Flikkema@student.tudelft.nl>
2015-05-29 11:33:43 +0200

    Resolve 'multiple main types' issue.

--- src/main/java/nl/tudelft/ti2806/pl1/main/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The main package containing the utility class Start. The main method in this class will run the program.
- * @author PL1
- *
- */
-package nl.tudelft.ti2806.pl1.main;
---
+++ src/main/java/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Utility class Start. The main method in this
+ * class will run the program.
+ * 
+ * @author PL1
+ *
+ */
