d49069f96ce52c79a6e329516a235fb1966e9a07
m4rissa <M.M.vanderWel@student.tudelft.nl>
2015-06-02 12:19:52 +0200

    First tests for new DGraph.

--- src/test/java/nl/tudelft/ti2806/pl1/DGraph/DGraphTest.java
+++ src/test/java/nl/tudelft/ti2806/pl1/DGraph/DGraphTest.java
@@ -3,12 +3,26 @@ package nl.tudelft.ti2806.pl1.DGraph;
 import static org.junit.Assert.assertEquals;
 
 import java.awt.Point;
+import java.io.IOException;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.neo4j.graphdb.MultipleFoundException;
 
 public class DGraphTest {
 
-	DGraph graph = new DGraph("src/test/resources/dgraphtestdb");
+	static DGraph graph;
+
+	@BeforeClass
+	public static void setUp() throws IOException {
+		graph = new DGraph("src/test/resources/dgraphtestdb");
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		graph = null;
+	}
 
 	@Test
 	public void addNewNodeTest() {
@@ -17,10 +31,17 @@ public class DGraphTest {
 		assertEquals(graph.getNodes().size(), 1);
 	}
 
-	@Test
+	@Test(expected = MultipleFoundException.class)
 	public void addExisitingTest() {
 		graph.addNode(0, 0, 2, "ATC", new Point(0, 0), 0,
 				new String[] { "TKK-REF" });
 	}
 
+	@Test
+	public void addNodeExistingSource() {
+		graph.addNode(1, 0, 5, "ATGCA", new Point(0, 0), 0,
+				new String[] { "TKK-ID2" });
+		assertEquals(graph.getNodes().size(), 2);
+		assertEquals(graph.getSources().size(), 2);
+	}
 }
