74ca483c0f460b7a63777f594ae673aefbefc6cd
justinvdh <J.R.T.E.vanderhout@student.tudelft.nl>
2015-06-15 09:14:46 +0200

    modify the node placing to change the view size

--- src/main/java/nl/tudelft/ti2806/pl1/zoomlevels/ZoomlevelCreator.java
+++ src/main/java/nl/tudelft/ti2806/pl1/zoomlevels/ZoomlevelCreator.java
@@ -1,10 +1,7 @@
 package nl.tudelft.ti2806.pl1.zoomlevels;
 
-import java.awt.Dimension;
-
 import nl.tudelft.ti2806.pl1.DGraph.ConvertDGraph;
 import nl.tudelft.ti2806.pl1.DGraph.DGraph;
-import nl.tudelft.ti2806.pl1.reader.NodePlacer;
 
 import org.graphstream.graph.Graph;
 
@@ -33,18 +30,15 @@ public final class ZoomlevelCreator {
 	 * 
 	 * @param threshold
 	 *            The score threshold.
-	 * @param viewSize
-	 *            The view size.
 	 * @return The created graph
 	 */
-	public Graph createGraph(final int threshold, final Dimension viewSize) {
+	public Graph createGraph(final int threshold) {
 		Graph ret = ConvertDGraph.convert(graph);
 		ret = InDelCollapser.collapseInsertions(graph.getInsmutations(), ret);
 		ret = InDelCollapser.collapseDeletions(graph.getDelmutations(), ret);
 		ret = PointGraphConverter.collapseNodes(graph.getAllPointMutations(),
 				ret, threshold);
 		ret = HorizontalCollapser.horizontalCollapse(ret);
-		NodePlacer.place(ret, viewSize);
 		return ret;
 	}
 }
