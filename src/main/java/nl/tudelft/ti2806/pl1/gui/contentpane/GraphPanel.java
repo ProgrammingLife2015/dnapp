9141b0f06ca6c594ffab52b005eaa8b593be205a
justinvdh <J.R.T.E.vanderhout@student.tudelft.nl>
2015-06-15 09:54:37 +0200

    add collapsing complex mutations

--- src/main/java/nl/tudelft/ti2806/pl1/gui/contentpane/GraphPanel.java
+++ src/main/java/nl/tudelft/ti2806/pl1/gui/contentpane/GraphPanel.java
@@ -266,6 +266,7 @@ public class GraphPanel extends JSplitPane implements ContentTab {
 				.findDeletionMutations(dgraph));
 		dgraph.setInsertionmutations(MutationFinder
 				.findInsertionMutations(dgraph));
+		dgraph.setComplexMutations(MutationFinder.findComplexMutations(dgraph));
 	}
 
 	/**
