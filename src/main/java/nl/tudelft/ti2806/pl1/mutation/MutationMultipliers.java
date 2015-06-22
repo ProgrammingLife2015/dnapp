c6cd3ae360b140f42ba00e4f121b765eeb789144
m4rissa <M.M.vanderWel@student.tudelft.nl>
2015-06-22 10:37:23 +0200

    Improve mutationmultipliers structure.

--- src/main/java/nl/tudelft/ti2806/pl1/mutation/MutationMultipliers.java
+++ src/main/java/nl/tudelft/ti2806/pl1/mutation/MutationMultipliers.java
@@ -7,53 +7,40 @@ package nl.tudelft.ti2806.pl1.mutation;
  * @since 18-06-15
  */
 public enum MutationMultipliers {
-
 	/** Multiplier for score if mutation is in a gene. */
-	IN_GENE {
-		@Override
-		public String getGUI() {
-			return "Mutation in Gene";
-		}
-	},
+	IN_GENE("Mutation in Gene"),
 
 	/** Multiplier for score of its a known mutation. */
-	KNOWN_MUTATION {
-
-		@Override
-		public String getGUI() {
-			return "Known Mutation";
-		}
-
-	},
+	KNOWN_MUTATION("Known Mutation"),
 
 	/** Importance INDELS. **/
-	INDEL {
-		@Override
-		public String getGUI() {
-			return "Importance INDELs";
-		}
-	},
+	INDEL("Importance INDELs"),
 
 	/** Importance Pointmutations. **/
-	POINTMUTATION {
-		@Override
-		public String getGUI() {
-			return "Importance Pointmutations";
-		}
-	},
+	POINTMUTATION("Importance Pointmutations"),
 	/** Use the Phylogenetic tree. **/
-	PHYLO {
-		@Override
-		public String getGUI() {
-			return "Use phylogenetic tree";
-		}
-	};
+	PHYLO("Use phylogenetic tree");
+
+	/** The GUI name for the multiplier. **/
+	private final String guiname;
+
+	/**
+	 * Score multipliers for the mutation.
+	 * 
+	 * @param guiname
+	 *            The GUI name for the multiplier.
+	 */
+	MutationMultipliers(final String guiname) {
+		this.guiname = guiname;
+	}
 
 	/**
 	 * Get the GUI name for the multiplier.
 	 * 
 	 * @return Name we want the GUI to use.
 	 */
-	public abstract String getGUI();
+	public String getGUI() {
+		return guiname;
+	}
 
 }
