be3735f3cc7ca25623b0d99027f45a399e136892
mpasterkamp <M.Pasterkamp@student.tudelft.nl>
2015-06-22 11:39:33 +0200

    added constructor to the enum to avoid duplicate code

--- src/main/java/nl/tudelft/ti2806/pl1/file/ImportDialog.java
+++ src/main/java/nl/tudelft/ti2806/pl1/file/ImportDialog.java
@@ -60,82 +60,52 @@ public class ImportDialog extends JFileChooser {
 		/**
 		 * Only graph node files will be shown.
 		 */
-		NODES {
-			@Override
-			public FileFilter getFileFilter() {
-				return new GraphNodeFileFilter();
-			}
-
-			@Override
-			public String getTitle() {
-				return "Load node file";
-			}
-		},
+		NODES(new GraphNodeFileFilter(), "Load node file"),
 
 		/** Only graph edge files will be shown. */
-		EDGES {
-			@Override
-			public FileFilter getFileFilter() {
-				return new GraphEdgeFileFilter();
-			}
-
-			@Override
-			public String getTitle() {
-				return "Load edge file";
-			}
-		},
+		EDGES(new GraphEdgeFileFilter(), "Load edge file"),
 
 		/** Only phylo files will be shown. */
-		PHYLO {
-			@Override
-			public FileFilter getFileFilter() {
-				return new ExtensionFileFilter("Newick file", "nwk");
-			}
-
-			@Override
-			public String getTitle() {
-				return "Load phylogenetic tree file";
-			}
-		},
+		PHYLO(new ExtensionFileFilter("Newick file", "nwk"),
+				"Load phylogenetic tree file"),
 
 		/** Only gene annotation files will be shown. */
-		GENE_ANNOTATION {
-			@Override
-			public FileFilter getFileFilter() {
-				return new ExtensionFileFilter("GFF file", "gff");
-			}
-
-			@Override
-			public String getTitle() {
-				return "Load gene annotation";
-			}
-		},
+		GENE_ANNOTATION(new ExtensionFileFilter("GFF file", "gff"),
+				"Load gene annotation"),
 
 		/** Only known resistance mutations will be shown. */
-		KNOWN_RES_MUT {
-			@Override
-			public FileFilter getFileFilter() {
-				return new ExtensionFileFilter(
-						"RCM file (resistance causing mutations)", "rcm");
-			}
+		KNOWN_RES_MUT(new ExtensionFileFilter(
+				"RCM file (resistance causing mutations)", "rcm"),
+				"Load resistance causing mutations file.");
 
-			@Override
-			public String getTitle() {
-				return "Load resistance causing mutations file.";
-			}
-		};
+		/**
+		 * The typefilter of the importType.
+		 */
+		private FileFilter filter;
+
+		/** The title for the file chooser dialog. */
+		private String title;
+
+		ImportType(final FileFilter inFilter, final String inTitle) {
+			filter = inFilter;
+			title = inTitle;
+		}
 
 		/**
 		 * @return a file filter accepting only files for this type of
 		 *         importable files.
 		 */
-		public abstract FileFilter getFileFilter();
+		public FileFilter getFileFilter() {
+			return filter;
+		}
 
 		/**
 		 * @return The title for the file chooser dialog this type of importable
 		 *         files.
 		 */
-		public abstract String getTitle();
+		public String getTitle() {
+			return title;
+		}
 	}
 
 }
